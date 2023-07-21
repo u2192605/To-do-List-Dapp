2301fcb7e23b79f263bb0f6421937c4b31b4fe51
MostafaXLotfy <mostafaXlotfy@gmail.com>
2023-07-21 18:21:13 +0300

    added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,19 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
+// import cors from 'cors'
+const cors = require('cors')
 
 let app: Express;
 export const initExpressApp = () => {
   app = express();
   app.use(bodyParser.json());
+  app.use(cors({
+    origin: 'http://localhost:3000'
+  }))
+
 };
 
 export const getExpressApp = (): Express => {
   return app;
 };
+
